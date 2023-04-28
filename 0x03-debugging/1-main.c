6407f99a481ddbb40909073c57c04769ce92abd0
Sakaisha <aishasakkary@outlook.com>
2023-04-27 19:37:13 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,21 +5,21 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-    int i;
+      int main(void)
+      {
+      int i;
 
-    printf("Infinite loop incoming :(\n");
+       printf("Infinite loop incoming :(\n");
 
-    i = 0;
-    /*
-     * while (i < 10)
-     * {
-     *     putchar(i);
-     * }
-     */
+       i = 0;
+       /*
+        * while (i < 10)
+        * {
+        *     putchar(i);
+        * }
+        */
 
-    printf("Infinite loop avoided! \\o/\n");
+       printf("Infinite loop avoided! \\o/\n");
 
-    return (0);
-}
+       return (0);
+       }
