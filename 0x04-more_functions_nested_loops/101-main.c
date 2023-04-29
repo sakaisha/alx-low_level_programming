4f7cbf3fa46d50f2faaeb31c765616cffa4c01be
Sakaisha <aishasakkary@outlook.com>
2023-04-28 18:18:45 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * The main function serves as the entry point of the program.
+ *
+ * @return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
