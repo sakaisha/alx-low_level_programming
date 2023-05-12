bf47994aece68bca0faf424e6a566894c854484a
Sakaisha <aishasakkary@outlook.com>
2023-05-12 09:56:43 -0700

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * main - arguments added to the program
- * @argc: number 
- * @argv: array 
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
