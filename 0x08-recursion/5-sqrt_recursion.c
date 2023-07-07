a38923c91e48fc42f37ad530f554509dde7fa4f6
Sakaisha <aishasakkary@outlook.com>
2023-07-06 19:33:32 -0700

    commit codes

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: number 
+ *
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recursing
+ * square root of a no
+ * @n: no to calculate the sqaure root
+ * @i: iterator
+ *
+ * Return: square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
