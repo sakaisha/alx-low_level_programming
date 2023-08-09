33b172e0a95455414a6b6608070e0409f26f4576
Sakaisha <aishasakkary@outlook.com>
2023-08-09 08:08:19 -0700

    Library creation please work

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -2,8 +2,8 @@
 
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
+ * _isalpha - checks for alphabetic char
+ * @c: the char to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
 	int _isalpha(int c)
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -2,10 +2,11 @@
 
 
 /**
- * _abs - computes the absolute value of an integer
+ * _abs - computes  absolute value of an integer
  * @n: the int to check
  * Return: the absolute value of int
  */
+
 	int _abs(int n)
 	{
 		if (n >= 0)
