851e9225f84a245ded1e4bf816a241b7cc02aff5
Sakaisha <aishasakkary@outlook.com>
2023-09-01 18:46:38 -0700

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int my_binary_to_uint(const char *binary_str)
 {
-    int index;
-    unsigned int decimal_value = 0;
+	int index;
+	unsigned int decimal_value = 0;
 
-    if (!binary_str)
-        return (0);
+	if (!binary_str)
+		return (0);
 
-    for (index = 0; binary_str[index]; index++)
-    {
-        if (binary_str[index] < '0' || binary_str[index] > '1')
-            return (0);
-        decimal_value = 2 * decimal_value + (binary_str[index] - '0');
-    }
+	for (index = 0; binary_str[index]; index++)
+	{
+		if (binary_str[index] < '0' || binary_str[index] > '1')
+			return (0);
+		decimal_value = 2 * decimal_value + (binary_str[index] - '0');
+	}
 
-    return (decimal_value);
+	return (decimal_value);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,17 +1,16 @@
-#include "custom_header.h"
+#include "main.h"
 
 /**
- * custom_set_bit - sets a bit at a given index to 1
- * @number: pointer to the number to change
- * @bit_index: index of the bit to set to 1
+ * updateBit - Changes the value of a bit at a specified position to 1.
+ * @num: Pointer to the number to be modified.
+ * @position: Position of the bit to set to 1.
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 for success, -1 for failure.
  */
-int custom_set_bit(unsigned long int *number, unsigned int bit_index)
-{
-    if (bit_index > 63)
-        return (-1);
+int updateBit(unsigned long int *num, unsigned int position)
+	if (position > 63)
+		return (-1);
 
-    *number = ((1UL << bit_index) | *number);
-    return (1);
+	*num = ((1UL << position) | *num);
+	return (1);
 }
