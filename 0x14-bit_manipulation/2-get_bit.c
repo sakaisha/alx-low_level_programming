4297707e40109da35a580012cee531df2dcd443a
Sakaisha <aishasakkary@outlook.com>
2023-09-01 19:06:52 -0700

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
-	bitValue = ((n >> index) & 1); 
+	bitValue = ((n >> index) & 1);
 
 	return (bitValue);
 }
