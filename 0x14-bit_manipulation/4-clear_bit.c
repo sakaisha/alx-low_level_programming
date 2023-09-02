034b70c97c0c69b0a2ff221a87da8ac85ce54bca
Sakaisha <aishasakkary@outlook.com>
2023-09-01 19:14:48 -0700

    fix last

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * change_bit - updates the value of a specified bit to 0
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: pointer to the number to be modified
  * @index: index of the bit to change
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 if it worked, or -1 if an error occurred
  */
-int change_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * count_bits_to_change - calculates the number of bits to change
- * in order to transform one number into another
+ * flip_bits - calculates the number of bits to change
+ * to transform one number into another.
  * @n: the first number
  * @m: the second number
  *
  * Return: the count of bits to change
  */
-unsigned int count_bits_to_change(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int current;
