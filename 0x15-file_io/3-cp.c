9e75f675e255861b7267dc303a2625582317b432
Sakaisha <aishasakkary@outlook.com>
2023-10-02 18:20:10 -0700

    what?

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,114 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#define BUF_SIZE 1024
-
-/**
- * open_files - Opens source and destination files.
- * @file_from: Source file name.
- * @file_to: Destination file name.
- *
- * Return: Array of file descriptors [source, destination].
- */
-int *open_files(char *file_from, char *file_to)
-{
-	int *fd = malloc(2 * sizeof(int));
-
-	fd[0] = open(file_from, O_RDONLY);
-	if (fd[0] < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	fd[1] = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd[1] < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		close(fd[0]);
-		exit(99);
-	}
-	return (fd);
-}
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * copy_content - Copies content from source to destination.
- * @fd_from: Source file descriptor.
- * @fd_to: Destination file descriptor.
+ * main - Copies the content of a file to another file.
+ * @ac: Number of arguments.
+ * @av: Array of arguments.
  *
- * Return: 0 on success, 98 on read error, 99 on write error.
+ * Return: 0 on success, otherwise exit with a specific code.
  */
-int copy_content(int fd_from, int fd_to)
+int main(int ac, char **av)
 {
-	ssize_t n;
-	char buf[BUF_SIZE];
+	int fd_from, fd_to, r, w;
+	char buffer[1024];
 
-	while ((n = read(fd_from, buf, BUF_SIZE)) > 0)
-	{
-		if (write(fd_to, buf, n) != n)
-		{
-			dprintf(2, "Error: Can't write to fd %d\n", fd_to);
-			return (99);
-		}
-	}
-	if (n < 0)
-	{
-		dprintf(2, "Error: Can't read from fd %d\n", fd_from);
-		return (98);
-	}
+	if (ac != 3)
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]), exit(97);
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+
+	do {
+		r = read(fd_from, buffer, 1024);
+		if (r == -1)
+			dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+			dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+	} while (r == 1024);
+
+	if (close(fd_from) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+	if (close(fd_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+
 	return (0);
-}
-
-/**
- * close_files - Closes file descriptors.
- * @fd_from: Source file descriptor.
- * @fd_to: Destination file descriptor.
- *
- * Return: 0 on success, 100 on close error.
- */
-int close_files(int fd_from, int fd_to)
-{
-	if (close(fd_from) < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		return (100);
-	}
-	if (close(fd_to) < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		return (100);
-	}
-	return (0);
-}
-
-/**
- * main - Entry point of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- *
- * Return: 97 for incorrect arguments, 98 for read error,
- *         99 for write error, or 100 for close error.
- */
-int main(int argc, char *argv[])
-{
-	int res;
-	int *fd;
-
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd = open_files(argv[1], argv[2]);
-	res = copy_content(fd[0], fd[1]);
-	if (res != 0)
-	{
-		close_files(fd[0], fd[1]);
-		exit(res);
-	}
-	res = close_files(fd[0], fd[1]);
-	free(fd);
-	return (res);
 }
