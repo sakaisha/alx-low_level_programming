e99b63f69843bb68b563b06db1c376af9266aa9c
Sakaisha <aishasakkary@outlook.com>
2023-11-14 14:23:19 -0800

    first

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,12 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+#include <stdio.h>
+
+size_t divid(size_t a, size_t b);
+size_t addition(size_t a, size_t b);
+size_t multi(size_t a, size_t b);
+size_t modif(size_t a, size_t b);
+size_t subtr(size_t a, size_t b);
+
+#endif
